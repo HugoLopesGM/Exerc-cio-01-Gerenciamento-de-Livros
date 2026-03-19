d5669a98d420551d6d838af17744d567587a74df
HugoLopesGM <hugolopesgarciamendes05@gmail.com>
2026-03-19 19:12:59 -0300

    repositorio

---
+++ ex1/src/main/java/com/GerenciamentodeLivros/ex1/repository/repositorioLivro.java
@@ -0,0 +1,9 @@
+package com.GerenciamentodeLivros.ex1.repository;
+
+import com.GerenciamentodeLivros.ex1.Model.Livro;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface repositorioLivro extends JpaRepository<Livro,Long> {
+}
